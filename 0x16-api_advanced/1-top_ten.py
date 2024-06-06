c236a25d37f98a717b7e6a17e580b2fd37a937be
Martinez <write2martinez@gmail.com>
2024-06-06 15:07:24 -0700

    1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,24 +1,38 @@
 #!/usr/bin/python3
 '''
-    this module contains the function top_ten
+    This module contains the function top_ten
 '''
 import requests
 from sys import argv
 
-
 def top_ten(subreddit):
     '''
-        returns the top ten posts for a given subreddit
+        Returns the top ten posts for a given subreddit
     '''
-    user = {'User-Agent': 'Lizzie'}
-    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
-                       .format(subreddit), headers=user).json()
+    user = {'User-Agent': 'Mozilla/5.0'}
+    url = f'https://www.reddit.com/r/{subreddit}/hot/.json?limit=10'
+    
     try:
-        for post in url.get('data').get('children'):
-            print(post.get('data').get('title'))
+        response = requests.get(url, headers=user, allow_redirects=False)
+        if response.status_code != 200:
+            print(None)
+            return
+
+        data = response.json()
+        posts = data.get('data', {}).get('children', [])
+        if not posts:
+            print(None)
+            return
+
+        for post in posts:
+            print(post.get('data', {}).get('title'))
+
     except Exception:
         print(None)
 
-
 if __name__ == "__main__":
-    top_ten(argv[1])
+    if len(argv) > 1:
+        top_ten(argv[1])
+    else:
+        print(None)
+
